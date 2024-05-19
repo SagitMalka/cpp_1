71dfcfa8891d6bfcc85c90ba2112a33f1a9d16cc
Sagit Malka <118733667+SagitMalka@users.noreply.github.com>
2024-05-19 10:17:48 +0300

    negativeCycle func

--- Algorithms.hpp
+++ Algorithms.hpp
@@ -15,16 +15,17 @@ namespace ariel{
     class Algorithms {
     private:
         static void traverse(int u, const vector<vector<int>>& adjacencyMatrix, unordered_set<int>& visited, size_t v_size);
-        static vector<int> bellmanFord(const Graph& , int source, int destination);
+        static vector<int> bellmanFord(const Graph& , int source);
         static ostringstream constructPath(int source, int dest, const std::vector<int>& parents);
         static string ConstructSets(const unordered_set<int>& setA, unordered_set<int> setB);
         static ostringstream constructCycle(stack<int>& vertices);
 
     public:
         static int isConnected(Graph& graph);
-        static std::string shortestPath(const Graph& g, int source, int vertex_v);
+        static string shortestPath(const Graph& g, int source, int vertex_v);
         static string isBipartite(const Graph& graph);
         static bool isContainsCycle(const Graph& graph);
+        static bool negativeCycle(const Graph& graph);
 
     };
 
